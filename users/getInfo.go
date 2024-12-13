5994858dd1710bae225b4ad31d72ca9019d915d9
AlexYTGamePL <unkor00t@gmail.com>
2024-12-13 10:23:11 +0100

    v00#017
    Changed getinfo, now you can chose rather you want to get profile back or not

--- users/getInfo.go
+++ users/getInfo.go
@@ -9,9 +9,16 @@ import (
 func GetInfo(session *gocql.Session) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		target := c.Param("userId")
+		var body struct {
+			Profile *bool `json:"profile,omitempty"`
+		}
 		if target == "" {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "userId parameter is required"})
 			return
+		}
+		err := c.BindJSON(&body)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		}
 		var user struct {
 			Createdat       string       `json:"createdat"`
@@ -22,27 +29,48 @@ func GetInfo(session *gocql.Session) gin.HandlerFunc {
 			BannerColor     string       `json:"bannercolor"`
 			BackgroundColor string       `json:"backgroundcolor"`
 			Status          int          `json:"status"`
-			Desc string `json:"desc"`
-			Profile []byte `json:"profile"`
+			Desc            string       `json:"desc"`
+			Profile         *[]byte      `json:"profile"`
 		}
-		if err := session.Query(
-			`SELECT createdat, user_id, username, badges, displayname, bannercolor, backgroundcolor, status, desc, profile FROM users WHERE user_id = ? ALLOW FILTERING`,
-			target,
-		).Scan(
-			&user.Createdat,
-			&user.UserId,
-			&user.Username,
-			&user.Badges,
-			&user.Displayname,
-			&user.BannerColor,
-			&user.BackgroundColor,
-			&user.Status,
-			&user.Desc,
-			&user.Profile,
-		); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
+		if body.Profile != nil && *body.Profile {
+			if err := session.Query(
+				`SELECT createdat, user_id, username, badges, displayname, bannercolor, backgroundcolor, status, desc, profile FROM users WHERE user_id = ? ALLOW FILTERING`,
+				target,
+			).Scan(
+				&user.Createdat,
+				&user.UserId,
+				&user.Username,
+				&user.Badges,
+				&user.Displayname,
+				&user.BannerColor,
+				&user.BackgroundColor,
+				&user.Status,
+				&user.Desc,
+				&user.Profile,
+			); err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				return
+			}
+		} else {
+			if err := session.Query(
+				`SELECT createdat, user_id, username, badges, displayname, bannercolor, backgroundcolor, status, desc FROM users WHERE user_id = ? ALLOW FILTERING`,
+				target,
+			).Scan(
+				&user.Createdat,
+				&user.UserId,
+				&user.Username,
+				&user.Badges,
+				&user.Displayname,
+				&user.BannerColor,
+				&user.BackgroundColor,
+				&user.Status,
+				&user.Desc,
+			); err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				return
+			}
 		}
+		c.Header("Content-Type", "application/json; charset=utf-8")
 		c.JSON(http.StatusOK, user)
 		return
 	}
