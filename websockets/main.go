fae78a70f231d39316676f42f6d7f6efc43c1483
UnkoR00T <99119433+AlexYTGamePL@users.noreply.github.com>
2024-12-18 20:30:56 +0100

    Hotfix v0.0#018.5

--- websockets/main.go
+++ websockets/main.go
@@ -104,18 +104,30 @@ func HandleMessageSendWS(server string, target string, message types.Message) {
 
 // HandleMessageDeleteWS notifies the target user of a deleted message
 func HandleMessageDeleteWS(server string, target string, message struct {
-	Target     gocql.UUID  `json:"TargetId"`
-	Server     *gocql.UUID `json:"ServerId"`
-	Message    gocql.UUID  `json:"MessageId"`
-	SentAt     string      `json:"SentAt"`
-	SentAtTime int64       `json:"SentAtTime"`
+	Target     gocql.UUID  `json:"target"`
+	Server     *gocql.UUID `json:"server"`
+	Message    gocql.UUID  `json:"message"`
+	SentAt     string      `json:"sentat"`
+	SentAtTime int64       `json:"sentattime"`
 }) {
 	if server == "" {
 		targetUUID, _ := gocql.ParseUUID(target)
 		if wsConn, exists := ConnactedUsers[targetUUID]; exists {
 			wsConn.WriteJSON(gin.H{
 				"type":    "delete_message",
-				"payload": message,
+				"payload": struct {
+					TargetId     gocql.UUID  `json:"TargetId"`
+					ServerId     *gocql.UUID `json:"ServerId"`
+					MessageId    gocql.UUID  `json:"MessageId"`
+					SentAt     string      `json:"SentAt"`
+					SentAtTime int64       `json:"SentAtTime"`
+				}{
+					TargetId: message.Target,
+					ServerId: message.Server,
+					MessageId: message.Message,
+					SentAt: message.SentAt,
+					SentAtTime: message.SentAtTime,
+				},
 			})
 		}
 	}
